b8dd7008e74a4dca6a7a2889837f14cc8571f420
caiyiheng <cyh@ejoy.com>
2018-03-19 10:27:16 +0800

    test some lib

--- js/draw_seq.js
+++ js/draw_seq.js
@@ -1,120 +1,53 @@
 import * as Const from './const'
 // import Matter from './libs/matter'
-import Box2D from './libs/Box2D'
+// import Box2D from './libs/Box2D'
+import * as THREE from './libs/three.min'
 
 
-let ctx = canvas.getContext('2d')
+let ctx = canvas.getContext('webgl')
 
 let instance
 
-let b2Vec2 = Box2D.Common.Math.b2Vec2;
-let b2BodyDef = Box2D.Dynamics.b2BodyDef;
-let b2Body = Box2D.Dynamics.b2Body;
-let b2FixtureDef = Box2D.Dynamics.b2FixtureDef;
-let b2Fixture = Box2D.Dynamics.b2Fixture;
-let b2World = Box2D.Dynamics.b2World;
-let b2MassData = Box2D.Collision.Shapes.b2MassData;
-let b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape;
-let b2CircleShape = Box2D.Collision.Shapes.b2CircleShape;
-let b2DebugDraw = Box2D.Dynamics.b2DebugDraw;
+// let b2Vec2 = Box2D.Common.Math.b2Vec2;
+// let b2BodyDef = Box2D.Dynamics.b2BodyDef;
+// let b2Body = Box2D.Dynamics.b2Body;
+// let b2FixtureDef = Box2D.Dynamics.b2FixtureDef;
+// let b2Fixture = Box2D.Dynamics.b2Fixture;
+// let b2World = Box2D.Dynamics.b2World;
+// let b2MassData = Box2D.Collision.Shapes.b2MassData;
+// let b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape;
+// let b2CircleShape = Box2D.Collision.Shapes.b2CircleShape;
+// let b2DebugDraw = Box2D.Dynamics.b2DebugDraw;
 
-// let Engine = Matter.Engine,
-//     Render = Matter.Render,
-//     Runner = Matter.Runner,
-//     Body = Matter.Body,
-//     Composites = Matter.Composites,
-//     Common = Matter.Common,
-//     Constraint = Matter.Constraint,
-//     MouseConstraint = Matter.MouseConstraint,
-//     Mouse = Matter.Mouse,
-//     World = Matter.World,
-//     Bodies = Matter.Bodies;
-
-// let engine = Engine.create({
-//         enableSleeping: true,
-//     })
-// let world = engine.world
-// let options = {
-//     width: canvas.width,
-//     height: canvas.height,
-//     showVelocity: true,
-//     enabled: true,
-//     background: 'aquamarine',
-//     wireframeBackground: 'aquamarine',
-// }
-// let render = Render.create({
-//     canvas: canvas,
-//     engine: engine,
-//     options: options,
-// })
-let gravity = new b2Vec2(0, 9.8)
-let world = new b2World(gravity, true)
+// let gravity = new b2Vec2(0, 9.8)
+// let world = new b2World(gravity, true)
 
 let w = canvas.width
 let h = canvas.height
-let line_w = 25
+
+var camera, scene, renderer;
+var geometry, material, mesh;
 
 let test_world = function() {
-    // // add bodies
-    // var group = Body.nextGroup(true),
-    //     particleOptions = { friction: 0.00001, collisionFilter: { group: group }, render: { visible: false }},
-    //     constraintOptions = { stiffness: 0.06 },
-    //     cloth = Composites.softBody(200, 200, 20, 12, 5, 5, false, 8, particleOptions, constraintOptions);
+    camera = new THREE.PerspectiveCamera( 70, w / h, 0.01, 10 );
+    camera.position.z = 1;
 
-    // for (var i = 0; i < 20; i++) {
-    //     cloth.bodies[i].isStatic = true;
-    // }
+    scene = new THREE.Scene();
 
-    // World.add(world, [
-    //     cloth,
-    //     Bodies.circle(300, 500, 80, { isStatic: true }),
-    //     Bodies.rectangle(500, 480, 80, 80, { isStatic: true }),
-    //     Bodies.rectangle(400, 609, 800, 50, { isStatic: true })
-    // ]);
-    // // let ball = Bodies.circle(w/2, line_w*4, 25, {
-    // //     density: 0.68, // 密度
-    // //     restitution: 0.8 // 弹性
-    // // });
-    // // World.add(world, ball)
+    geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+    material = new THREE.MeshNormalMaterial();
 
-    // // // add mouse control
-    // // let mouse = Mouse.create(render.canvas)
-    // // let mouseConstraint = MouseConstraint.create(engine, {
-    // //     mouse: mouse,
-    // //     constraint: {
-    // //         stiffness: 0.2,
-    // //         render: {
-    // //             visible: false,
-    // //         }
-    // //     }
-    // // })
-    // // World.add(world, mouseConstraint)
-    // // render.mouse = mouse
+    mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
 
-    // // console.log(w, h)
-    // // World.add(world, [
-    // //     // walls
-    // //     Bodies.rectangle(w/2, line_w/2, w, line_w, { isStatic: true }),
-    // //     Bodies.rectangle(line_w/2, h/2, line_w, h, { isStatic: true }),
-    // //     Bodies.rectangle(w/2, h-line_w/2, w, line_w, { isStatic: true }),
-    // //     Bodies.rectangle(w-line_w/2, h/2, line_w, h, { isStatic: true })
-    // // ]);
-
-    // Engine.run(engine)
-
-    // // (function(render) {
-    // //     (function loop(time){
-    // //         render.frameRequestId = window.requestAnimationFrame(loop);
-    // //         Render.world(render);
-    // //     })();
-    // // })(render);
-    // Render.run(render)
+    renderer = new THREE.WebGLRenderer( { antialias: true, context: ctx } );
+    renderer.setSize( w, h );
+    document.body.appendChild( renderer.domElement );
 }
 
 /**
  * 界面绘图器
  */
-let lastFrame = new Date().getTime()
 export default class DrawSeq {
     constructor() {
         if (instance) {
@@ -138,11 +71,10 @@ export default class DrawSeq {
 
     update(score) {
         this.score = score
-        var tm = new Date().getTime()
-        var dt = (tm - lastFrame) / 1000
-        if (dt > 1/15) { dt = 1/15; }
-        world.Step(dt)
-        lastFrame = tm
+        // world.Step(1/60, 8, 3)
+        mesh.rotation.x += 0.01;
+        mesh.rotation.y += 0.02;
+        renderer.render( scene, camera );
     }
 
     draw(databus) {
--- js/libs/Box2D.js
+++ js/libs/Box2D.js
@@ -10864,4 +10864,5 @@ Box2D.postDefs = [];
 var i;
 for (i = 0; i < Box2D.postDefs.length; ++i) Box2D.postDefs[i]();
 delete Box2D.postDefs;
-export {Box2D}
+
+module.exports = Box2D
